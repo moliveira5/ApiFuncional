dce4540a45f50fc60ac8c23dab10b69f0c25cc54
Marina Guimarães <57577355+moliveira5@users.noreply.github.com>
2024-10-17 07:56:13 -0300

    Add files via upload

---
+++ Dados/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AplicacoesAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AplicacoesAPI.Dados
+{
+    public class ApiDbContext : IdentityDbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
